0fbbd5c29ef6237aa7e50f35560c06fa60174f6e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2025-06-22 22:10:04 +0530

    feat: improved ui

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -27,9 +27,6 @@ export default async function EditPage({ params }: any) {
 
   return (
     <section className="max-w-3xl mx-auto px-4 py-10">
-      <h1 className="text-3xl font-bold text-gray-900 dark:text-gray-100 mb-8">
-        Edit Blog Post
-      </h1>
       <PostForm initial={initialData} postId={id} />
     </section>
   );
